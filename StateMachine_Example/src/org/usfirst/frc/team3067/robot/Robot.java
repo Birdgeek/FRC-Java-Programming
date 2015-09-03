f8231956eb7bddbb899dae1be12e76a1a964ea0a
Brad <birdgeekthree@gmail.com>
2015-09-03 17:46:13 -0500

    End of Session One
    Covered some topics - Made some code.

--- StateMachine_Example/src/org/usfirst/frc/team3067/robot/Robot.java
+++ StateMachine_Example/src/org/usfirst/frc/team3067/robot/Robot.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3067.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
 
 
 public class Robot extends IterativeRobot {
@@ -10,6 +11,10 @@ public class Robot extends IterativeRobot {
 	public enum roboState { OPEN, CLAMPED, LIFTED };
 	roboState state;
 	Joystick rightStick = new Joystick(1);
+	Solenoid liftUp = new Solenoid(0);
+	Solenoid liftDown = new Solenoid(1);
+	Solenoid clampClose = new Solenoid(2);
+	Solenoid clampOpen = new Solenoid(3);
 	
     public void robotInit() {
 
@@ -28,37 +33,51 @@ public class Robot extends IterativeRobot {
     		switch (state) {
 			case CLAMPED:
 				state = roboState.LIFTED;
+				updateState();
 				break;
 			case LIFTED:
 				state = roboState.OPEN;
+				updateState();
 				break;
 			case OPEN:
 				state = roboState.CLAMPED;
+				updateState();
 				break;
 			default:
 				state = roboState.OPEN;
+				updateState();
 				break;
     		
     		}
     	}
-    	
-    	
-        switch (state) {
-		case CLAMPED:
-			break;
-		case LIFTED:
-			break;
-		case OPEN:
-			break;
-		default:
-			break;
-        
-        	
-        }
+
     }
     
     
-    public void testPeriodic() {
+    private void updateState() {    	    	
+        
+    	switch (state) {
+		case CLAMPED:
+			clampOpen.set(false);
+			clampClose.set(true);
+			break;
+		case LIFTED:
+			liftUp.set(true);
+			liftDown.set(false);
+			break;
+		case OPEN:
+			liftDown.set(true);
+			liftUp.set(false);
+			clampOpen.set(true);
+			clampClose.set(false);			
+			break;
+		default:
+			break;                
+        }		
+	}
+
+
+	public void testPeriodic() {
     
     }
     
